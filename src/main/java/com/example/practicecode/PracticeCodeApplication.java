3e91f588691595539825bd8695b75c909c0b4519
hflug8201 <jygassa@gmail.com>
2022-09-24 00:01:19 +0900

    first commit

---
+++ src/main/java/com/example/practicecode/PracticeCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.practicecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeCodeApplication.class, args);
+    }
+
+}
